4b93909922b7f99b5fcab1f27ca7ae966a2b3c52
Dirc <e.e.cornet@gmail.com>
2026-06-23 22:32:37 +0200

    Fix linter issues in main.go

--- main.go
+++ main.go
@@ -14,7 +14,11 @@ func main() {
 	router.HandleFunc("/env", env)
 	http.Handle("/", router)
 
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Server failed: %v\n", err)
+		os.Exit(1)
+	}
 }
 
 func hostname(w http.ResponseWriter, r *http.Request) {
@@ -24,14 +28,21 @@ func hostname(w http.ResponseWriter, r *http.Request) {
 		os.Exit(1)
 	}
 	fmt.Printf("Hostname: %s\n", hostname)
-	fmt.Fprintf(w, "Hostname: %s", hostname)
+	_, err = fmt.Fprintf(w, "Hostname: %s", hostname)
+	if err != nil {
+		fmt.Printf("Failed to write response: %v\n", err)
+	}
 }
 
 func env(w http.ResponseWriter, r *http.Request) {
 	env := os.Getenv("FOO")
+	var err error
 	if env == "" {
-		fmt.Fprintf(w, "Environment variable FOO is not found")
+		_, err = fmt.Fprintf(w, "Environment variable FOO is not found")
 	} else {
-		fmt.Fprintf(w, "FOO=%s", env)
+		_, err = fmt.Fprintf(w, "FOO=%s", env)
+	}
+	if err != nil {
+		fmt.Printf("Failed to write response: %v\n", err)
 	}
 }
